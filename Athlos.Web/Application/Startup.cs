89cf2fcce9f63d796c79ee86ff8b0b8b12ebcdde
Pieter Deparcq <pdeparcq@gmail.com>
2019-10-29 21:49:26 +0100

    Added Swashbuckle

--- Athlos.Web/Application/Startup.cs
+++ Athlos.Web/Application/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Athlos.Application
 {
@@ -18,6 +20,13 @@ namespace Athlos.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Athos API", Version = "v1" });
+            });
+
             services.AddControllers();
         }
 
@@ -38,6 +47,13 @@ namespace Athlos.Application
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Athos API V1");
             });
         }
     }
--- Athlos.Web/Application/WeatherForecastController.cs
+++ Athlos.Web/Application/WeatherForecastController.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 namespace Athlos.Application
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
